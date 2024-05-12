f037dcd6f4b90e18e1a708275cf6e87e112eb41f
Shahriyar <chyshahriyar@gmail.com>
2024-05-13 02:10:59 +0600

    Deleted job post from database using id

--- index.js
+++ index.js
@@ -61,6 +61,14 @@ async function run() {
             res.send(result);
         })
 
+        // Data delete in jobs database by id
+        app.delete('/joblist/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await alljobscollection.deleteOne(query);
+            res.send(result);
+        })
+
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
